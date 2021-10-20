1051f8ee303efa8fd5932144635e45b1405c01c1
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-20 22:13:07 +0530

    BASE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: any) => {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,38 @@
 import Head from 'next/head'
+import Card from '../components/Card'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>HBD NICKO!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+    <div className='bg-red-500 py-24 flex items-center justify-center'>
+      <h2 className='text-6xl text-white top-0'>
+        🎉 Happy Birthday Nicko!
+        </h2>
+    </div>
+    <div className='bg-red-500 flex flex-wrap justify-center space-y-3'>
+      <Card name='Yuu' head='Happy Birthday Nicko!' img='/1.png'>
+        You are 17 years old now.<br/>
+        I sincerely wish you success and happiness in your future life.<br/>
+        You're a great person!<br/>
+        Thanks for becoming my friend & Brawl stars partner
+        Happy Birthday again<br/>
+        Enjoy your day! Love ya Rem<br/>
+      </Card>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Card name='Alen' head="Get JoJo'd" img='/2.png'>
+       Well... It's "that" time of the year again...<br/>
+       It's your Birthday! Not Just Another Birthday, You're turning 17!<br/>
+       I'm so happy for you!<br/>
+       So yeah, I'm gonna get you a JoJo!<br/>
+       Yes, you heard me right, I'm gonna get you a JoJo!<br/>
+      Jokes aside, I'm really gald that you're my friend.
+      So let's hope for the best. Have a great day & year!
+      </Card>
+      </div>
     </div>
   )
 }
